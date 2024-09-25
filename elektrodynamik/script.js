8bc59df2f3f4c3a762f08fd805a8cde581eac91e
Leon_lp9 <73760817+Leonlp9@users.noreply.github.com>
2024-09-25 20:20:10 +0200

    Update

--- elektrodynamik/script.js
+++ elektrodynamik/script.js
@@ -114,40 +114,6 @@ function drawElement(canvas, x, y, width, height, color, text) {
 }
 
 /**
- * Erstellt einen Button
- * @param parent Element, in dem der Button erstellt werden soll
- * @param text Text des Buttons
- * @param onClick Funktion, die aufgerufen wird, wenn der Button geklickt wird
- */
-function createButton(parent, text, onClick) {
-    const button = document.createElement("button");
-    button.innerText = text;
-    button.onclick = onClick;
-    button.classList.add("button");
-    parent.appendChild(button);
-}
-
-/**
- * Erstellt einen Slider
- * @param parent Element, in dem der Slider erstellt werden soll
- * @param min Mindestwert
- * @param max Maximalwert
- * @param value Startwert
- * @param step Schrittweite
- * @param onChange Funktion, die aufgerufen wird, wenn sich der Wert ändert
- */
-function createSlider(parent, min, max, value, step, onChange) {
-    const slider = document.createElement("input");
-    slider.type = "range";
-    slider.min = min;
-    slider.max = max;
-    slider.value = value;
-    slider.step = step;
-    slider.oninput = onChange;
-    parent.appendChild(slider);
-}
-
-/**
  * Erstellt eine Animation für die Radiallinie
  * @param elementId
  */
